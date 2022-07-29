9dc5d5c367d19d7671b136d3abb1aa250107d630
MSI\gn01792218 <gn01792218@hotmail.com>
2022-07-29 15:59:42 +0800

    使用useAsyncData來撰寫同步商業邏輯

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="h-screen relative overflow-hidden">
-        <img src="" alt="">
+        <img :src="bgUrl" alt="">
         <div class="absolute w-full h-full top-0 overlay">
         </div>
         <div class="absolute w-full h-full top-0 p-48">
             <WeaterInfo
-            v-if="data"
+            v-if="weater"
             :weather-data="{
-                cityName:data.city.name,
-                country:data.city.country,
+                cityName:weater.city.name,
+                country:weater.city.country,
                 date:date,
-                imageIcon:data.list[0].weather[0].icon,
-                temprature:data.list[0].main.temp,
+                imageIcon:weater.list[0].weather[0].icon,
+                temprature:weater.list[0].main.temp,
             }"
             />
             <div v-else>找不到該地區的天氣資料</div>
@@ -24,16 +24,26 @@
 </template>
 <script lang="ts" setup>
 import { onMounted } from 'vue'
-import { useFetch, useRuntimeConfig } from '#app'
+import { useFetch, useRuntimeConfig, useAsyncData } from '#app'
 const config = useRuntimeConfig();
 const date = ref(new Date())
+const bgUrl = ref("")
 const searchInput = ref<string>("")
 const city = ref<string>("Toronto")
 const myApiKey = config.WEATER_API_KEY
 const weatherApi = computed(()=>{
     return `https://api.openweathermap.org/data/2.5/forecast?q=${city.value}&units=metric&appid=${myApiKey}`
 })
-const { data, error } =useFetch(()=>weatherApi.value) //使用箭頭函式寫法，只要weatherApi有變化，就會自動執行，並回傳新網址
+const { data:weater, error} = useAsyncData('getWeatherData',async ()=>{
+    const res = await $fetch(weatherApi.value)
+    const temp = res.list[0].main.temp
+    if(temp <= 20) bgUrl.value = "https://picsum.photos/id/684/600/400"
+    else if (temp > 20 && temp <= 25) bgUrl.value = "https://picsum.photos/id/683/600/400"
+    else bgUrl.value = "https://picsum.photos/id/81/600/400"
+    return res
+},{
+    watch:[city]
+})
 function handleWeatherSearch(){
     city.value = searchInput.value
 }
